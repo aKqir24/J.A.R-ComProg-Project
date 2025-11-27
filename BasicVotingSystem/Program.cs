368e16cfae02d7b4abc67289bb6c0a61ec889f0f
aKqir24 <Akqir@protonmail.com>
2025-11-28 02:07:26 +0800

    planning it for now

---
+++ BasicVotingSystem/Program.cs
@@ -0,0 +1,7 @@
+﻿# Ascii voter program art here:)
+# Put the candidates here gian, eg. president,vice
+# Assign these candidates into a variable for show
+# Identify if they are a voter or a scrutineer <-- this can be done by him
+# Display the candidates in a ascii table						^
+# Calculate the votes each category and identify the winer -----|
+
