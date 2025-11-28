9b09d3ef9d42c56be9b729ff24f189cf7dcaf125
aKqir24 <Akqir@protonmail.com>
2025-11-28 20:03:25 +0800

    update: welcome screen

--- BasicVotingSystem/Program.cs
+++ BasicVotingSystem/Program.cs
@@ -5,3 +5,30 @@
 // Display the candidates in a ascii table						^
 // Calculate the votes each category and identify the winer -----|
 
+using System;
+
+namespace SchoolVote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Welcome the 'User or Voter' when using this program!!
+            Console.WriteLine("                _                 _  _                       ");
+            Console.WriteLine("       ()      | |               | |(_|   |_/                ");
+            Console.WriteLine("       /\\  __  | |      __   __  | |  |   | __ _|_  _       ");
+            Console.WriteLine("      /  \\/    |/ \\    /  \\_/  \\_|/   |   |/  \\_|  |/   ");
+            Console.WriteLine("     /(__/\\___/|   |_\\/\\__/ \\__/ |__/  \\_/ \\__/ |_/|__/");
+            Console.WriteLine("\nA voting system made in C#, which focuses on managing the votes");
+            Console.WriteLine("               happening inside the school.\n");
+
+            // Ask you the user to pick an option to vote or manage the votes
+            Console.WriteLine("==================== Select an option: ======================\n");
+            Console.WriteLine("         1. Vote     2. Admin Panel      3. Exit             \n");
+            Console.Write("Your selection: ");
+            int option=Convert.ToInt32(Console.ReadLine());
+
+            // Identify the options then its statement
+        }
+    }
+}
